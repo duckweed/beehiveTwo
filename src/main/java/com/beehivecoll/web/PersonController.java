02e00935d036cd76849bffd5d58454e6716798f0
Andrew Oxenburgh <andrew.oxenburgh@adscale.co.nz>
2012-05-31 13:29:08 +1200

    first commit

---
+++ src/main/java/com/beehivecoll/web/PersonController.java
@@ -0,0 +1,12 @@
+package com.beehivecoll.web;
+
+import com.beehivecoll.domain.Person;
+import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/people")
+@Controller
+@RooWebScaffold(path = "people", formBackingObject = Person.class)
+public class PersonController {
+}
